b9af79cf601a88accc81db7ef734532bc93d3b3a
Deekshith SN <deekshithsn@gmail.com>
2022-09-16 07:19:38 +0530

    Create bool.tf

---
+++ Terraform/Variables/bool.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+   region     = "eu-central-1"
+}
+
+
+resource "aws_instance" "ec2_example" {
+
+   ami           = "ami-0767046d1677be5a0"
+   instance_type =  "t2.micro"
+   count = 1
+  associate_public_ip_address = var.enable_public_ip
+
+   tags = {
+           Name = "Terraform EC2"
+   }
+
+}
+
+variable "enable_public_ip" {
+  description = "Enable public IP address"
+  type        = bool
+  default     = true
+}
