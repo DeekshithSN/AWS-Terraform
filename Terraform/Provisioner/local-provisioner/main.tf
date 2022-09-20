495e64d7d9afcade0fa55b7353c048915b952de4
Deekshith SN <deekshithsn@gmail.com>
2022-09-20 19:48:27 +0530

    Create main.tf

---
+++ Terraform/Provisioner/local-provisioner/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+   region     = "eu-central-1"
+   access_key = "AKIATQ37NXBxxxxxxxxx"
+   secret_key = "JzZKiCia2vjbq4zGGGewdbOhnacmxxxxxxxxxxxx"
+   
+}
+
+resource "aws_instance" "ec2_example" {
+
+    ami = "ami-0767046d1677be5a0"  
+    instance_type = "t2.micro" 
+    tags = {
+        Name = "Terraform EC2"
+    }
+
+  provisioner "local-exec" {
+    command = "touch hello-jhooq.txt"
+  }
+}
