a34d818d45ccf706dcae9b10a38032bb99b98bdc
Deekshith SN <deekshithsn@gmail.com>
2022-09-24 17:15:20 +0530

    Update main.tf

--- Terraform/module-demo/modules/ec2-module-interpollation/main.tf
+++ Terraform/module-demo/modules/ec2-module-interpollation/main.tf
@@ -18,6 +18,7 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "app" {
   ami           = "${data.aws_ami.app_ami.id}"
   instance_type = var.instance_type
+  subnet_id = var.subnet-id
   tags = {
     Name = var.instance_name
     Env = var.environment
