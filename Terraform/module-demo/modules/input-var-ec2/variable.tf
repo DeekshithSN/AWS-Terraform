3c98b9eb061410d54f2faf3d7a4bbd54dc8c8759
Deekshith SN <deekshithsn@gmail.com>
2022-09-24 16:46:57 +0530

    Create variable.tf

---
+++ Terraform/module-demo/modules/input-var-ec2/variable.tf
@@ -0,0 +1,14 @@
+variable "instance_type" {
+   description = "Instance type t2.micro"
+   type        = string
+}
+
+variable "instance_name" {
+   description = "Instance type t2.micro"
+   type        = string
+}
+
+variable "environment" {
+   description = "Instance type t2.micro"
+   type        = string
+}
