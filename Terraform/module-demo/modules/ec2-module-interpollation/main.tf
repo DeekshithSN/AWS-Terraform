36513df4d92f99f23342e32b1f7cb674046899f8
Deekshith SN <deekshithsn@gmail.com>
2022-09-24 17:13:51 +0530

    Create main.tf

---
+++ Terraform/module-demo/modules/ec2-module-interpollation/main.tf
@@ -0,0 +1,25 @@
+
+data "aws_ami" "app_ami" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  
+  
+   filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-gp2"]
+  } 
+ 
+}
+
+resource "aws_instance" "app" {
+  ami           = "${data.aws_ami.app_ami.id}"
+  instance_type = var.instance_type
+  tags = {
+    Name = var.instance_name
+    Env = var.environment
+   }
+}
