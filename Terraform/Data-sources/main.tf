9985e324b0bb19ba1c444ce2fffcf2347e350707
Deekshith SN <deekshithsn@gmail.com>
2022-09-24 15:55:36 +0530

    Create main.tf

---
+++ Terraform/Data-sources/main.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "app_ami" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  
+   filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+  
+   filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-gp2"]
+  } 
+ 
+}
+
+resource "aws_instance" "app" {
+  ami           = "${data.aws_ami.app_ami.id}"
+  instance_type = "t2.micro"
+}
