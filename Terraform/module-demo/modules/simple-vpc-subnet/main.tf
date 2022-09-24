509a87550d3bb800f19867f1770252a8b8d5b243
Deekshith SN <deekshithsn@gmail.com>
2022-09-24 17:57:04 +0530

    Update main.tf

--- Terraform/module-demo/modules/simple-vpc-subnet/main.tf
+++ Terraform/module-demo/modules/simple-vpc-subnet/main.tf
@@ -9,6 +9,7 @@ resource "aws_vpc" "staging-vpc" {
 resource "aws_subnet" "staging-subnet" {
   vpc_id = aws_vpc.staging-vpc.id
   cidr_block = var.cidir_block
+  availability_zone = "ap-south-1a"
 
   tags = {
     Name = var.name
