f5575517dad9b99fc5f438854342f592b644ac98
Deekshith SN <deekshithsn@gmail.com>
2022-09-26 12:29:25 +0530

    Update 01.machine_stop_notify.py

--- Lamda_Functions/01.machine_stop_notify.py
+++ Lamda_Functions/01.machine_stop_notify.py
@@ -1,1 +1,8 @@
+import boto3
+client = boto3.client('sns')
 
+def lambda_handler(event, context):
+    Instance_id = event['detail']['instance-id']
+    topic_arn = 'arn_of_sns_alerts'
+    message = 'your server ' + Instance_id + 'is down'
+    client.publish(TopicArn=topic_arn,Message=message)
